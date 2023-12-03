49b8b59f623c6a1a14311b9e0a00dc2130f5c562
obeday01 <obeday01@gmail.com>
2023-12-02 22:16:19 -0500

    Done

--- highrollerheaven/src/Components/NavBarComponent/Navbar.js
+++ highrollerheaven/src/Components/NavBarComponent/Navbar.js
@@ -76,7 +76,6 @@ const Navbar = () => {
       {user && (
         <>
           <div className="top-navbar">
-            <img src={hrhLogo} alt="High Roller Heaven Logo" className="logo" />
             <div className="user-name">{user.username}</div>
             <img src={hrhLogo} alt="High Roller Heaven Logo" className="logo" />
             <Link to="/login" className="rigthalign">
